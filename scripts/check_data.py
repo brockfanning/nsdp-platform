5cd2a10b29c094fe90f5037488ef9fa90d96b194
Brock Fanning <brockfanning@gmail.com>
2021-04-29 21:51:33 -0400

    Fix inputs 2

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,8 +1,7 @@
 from sdg.open_sdg import open_sdg_check
-from inputs import get_inputs
 
 # Validate the indicators.
-validation_successful = open_sdg_check(config='config_data.yml', inputs=get_inputs())
+validation_successful = open_sdg_check(config='config_data.yml')
 
 # If everything was valid, perform the build.
 if not validation_successful:
