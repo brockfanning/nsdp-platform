d457b14796f1fc667c26014d383d09e0660f1059
Brock Fanning <brockfanning@gmail.com>
2021-05-05 22:16:29 -0400

    Script cleanup and reporting status fix

---
+++ scripts/alter_data.py
@@ -0,0 +1,5 @@
+def alter_data(df):
+    # Perform any alterations on the Pandas DataFrame "df".
+    # For example, to drop the "education" column you could use:
+    # df = df.drop(columns=['education'])
+    return df
--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,4 +1,5 @@
 from sdg.open_sdg import open_sdg_build
-from inputs import alter_meta
+from alter_meta import alter_meta
+from alter_data import alter_data
 
-open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
+open_sdg_build(config='config_data.yml', alter_meta=alter_meta, alter_data=alter_data)
