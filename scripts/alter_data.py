57fa32376cf6cf65e4f84ae09389c0cefc992c83
Brock Fanning <brockfanning@gmail.com>
2021-05-05 22:44:44 -0400

    More automated metadata

--- scripts/alter_data.py
+++ scripts/alter_data.py
@@ -1,5 +1,12 @@
-def alter_data(df):
-    # Perform any alterations on the Pandas DataFrame "df".
-    # For example, to drop the "education" column you could use:
-    # df = df.drop(columns=['education'])
+def alter_data(df, context):
+    """
+    Perform any alterations here on the Pandas DataFrame "df".
+
+    For example, here is how you might drop the "education" column
+    from indicator ENV1-3-2:
+
+    if context['indicator_id'] == 'ENV1-3-2':
+        df = df.drop(columns=['education'])
+    """
+    # Don't forget to return the DataFrame at the end:
     return df
--- scripts/alter_meta.py
+++ scripts/alter_meta.py
@@ -1,12 +1,41 @@
-def alter_meta(meta):
-    if 'NSDPIndicatorID' in meta:
-        # Auto-calculate settings based on 'NSDPIndicatorID'.
-        open_sdg_id = meta['NSDPIndicatorID'].replace(' ', '').replace('.', '-')
-        meta['indicator_number'] = open_sdg_id
-        meta['goal_number'] = open_sdg_id.split('-')[0]
+def get_pillar_code(goal_id):
+    if 'ENV' in goal_id:
+        return 'ENV'
+    if 'ECO' in goal_id:
+        return 'ECO'
+    if 'SOC' in goal_id:
+        return 'SOC'
+    return ''
+
+def alter_meta(meta, context):
+    # Auto-calculate settings based on the indicator id.
+    indicator_id = context['indicator_id']
+    indicator_id_parts = indicator_id.split('-')
+    if len(indicator_id_parts) == 3:
+
+        # These are used by Open SDG.
+        meta['indicator_number'] = indicator_id
+        meta['goal_number'] = indicator_id_parts[0]
         meta['goal_name'] = 'global_goals.' + meta['goal_number'] + '-title'
-        meta['target_number'] = open_sdg_id.split('-')[0] + '-' + open_sdg_id.split('-')[1]
+        meta['target_number'] = indicator_id_parts[0] + '-' + indicator_id_parts[1]
         meta['target_name'] = 'global_targets.' + meta['target_number'] + '-title'
-        meta['indicator_name'] = 'indicators.' + open_sdg_id + '-title'
-        meta['national_geographical_coverage'] = 'Vanuatu'
+        meta['indicator_name'] = 'indicators.' + indicator_id + '-title'
+
+        # These are used in the Vanuatu national metadata tab.
+        national_meta = {
+            'PillarCode': get_pillar_code(meta['goal_number']),
+            'PillarDescription': 'pillars.' + get_pillar_code(meta['goal_number']),
+            'GoalId': meta['goal_number'],
+            'GoalDescription': meta['goal_name'],
+            'PolicyObjectiveId': meta['target_number'],
+            'PolicyObjectiveDescription': meta['target_name'],
+            'IndicatorId': indicator_id,
+            'IndicatorDescription': meta['indicator_name'],
+        }
+        # Use these national defaults if nothing is set.
+        for key in national_meta:
+            if key not in meta or meta[key] == '' or meta[key] is None:
+                meta[key] = national_meta[key]
+
+    meta['national_geographical_coverage'] = 'Vanuatu'
     return meta
